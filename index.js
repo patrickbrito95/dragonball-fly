3cd2d21bd1390358da16d40c7ccfb052b18a8e18
Patrick Brito <patrickbrito95@gmail.com>
2020-11-03 18:01:05 -0300

    add score and ground

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 
-//DOM
+    //DOM
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-//START GAME
+    //START GAME
 
 document.getElementById('start-button').onclick = function (){
 document.getElementById('canvas-intro').style.display = "none";
 
 document.getElementById('canvas').style.display = "block";
 
+
 }
 
-// IMAGES
+    // IMAGES
 
 const img = new Image();
 img.src = './images/cenario2.png';
@@ -21,11 +22,11 @@ img.src = './images/cenario2.png';
 const goku = new Image();
 goku.src = './images/goku.png';
 
-const kiBlast = new Image();
-kiBlast.src = './images/ki-blast.gif';
+const blast = new Image();
+blast.src = './images/blast.png';
 
 
-// SONS
+    // SONS
 
 const explositon = new Audio();
 explositon.src = './sounds/explosion.mp3';
@@ -34,42 +35,7 @@ const music = new Audio();
 music.src = './sounds/we-gotta-power.mp3';
 
 
-
-// VARIÁVEIS
-
-
-
-
-
-
-//PERSONAGEM 
-
-
-const personagem = {
-  goku: goku,
-  y: 0,
-  x: 0,
-  gravity: -0.001,
-
-  move: function() {
-    this.y -= this.gravity--;
-  },
-
-  draw: function() {
-   ctx.drawImage(this.goku, 50, this.y + this.goku.height, 50, 54);
-  },
-
-};
-
-function drawGoku() {
-    personagem.move();
-    personagem.draw();
-    music.play();
-    requestAnimationFrame(drawGoku);
-}
-   window.onload = drawGoku;
-  
-  //  CRIAÇÃO DO BACKGROUND DO JOGO
+   //  CRIAÇÃO DO BACKGROUND DO JOGO
 
 const backgroundImage = {
   img: img,
@@ -93,22 +59,67 @@ const backgroundImage = {
 
 function updateCanvas() {
     backgroundImage.move();
-  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     backgroundImage.draw();
   
     requestAnimationFrame(updateCanvas);
   }
-  
-  img.onload = updateCanvas;
-  
- // OBSTACULOS
+
  
- 
+    //PERSONAGEM 
+
+
+const personagem = {
+  goku: goku,
+  y: 0,
+  x: 0,
+  gravity: -1,
+  speedX: 0,
+  speedY: 0,
+
+  move: function() {
+    this.y -= this.gravity--;  
+    
+  },
+
+  draw: function() {
+   ctx.drawImage(this.goku, 50, this.y + this.goku.height, 95, 90);
+  },
+  
+  limits: function(){
+    if(this.y >= canvas.height - goku.height - 90){
+      this.gravity = 0;
+    }
+  },
+    
+  };
+
+
+function drawGoku() {
+    personagem.move();
+    personagem.draw();
+    personagem.limits();
+    // music.play();
+    requestAnimationFrame(drawGoku);
+}
+  
+  //OBSTACULOS
+
+
+  
+    
+    
+    goku.onload = drawGoku;
+    window.onload = updateObstacle;
+    img.onload = updateCanvas;
+    
+  
 
 
 
-  // CONTROLES
+
+
+
+    //CONTROLES
 
 document.addEventListener('keydown', function(e){
   if(e.key === " "){
@@ -125,8 +136,106 @@ document.addEventListener('keyup', function(e){
     console.log(e.key);  
 });
 
-//SCORE
+  
+    // COLISÃO E OBSTÁCULOS
 
+    // class Component {
+    //   constructor(width, height, image, x, y) {
+    //     this.image = new Image();
+    //     this.image.src = image;
+    //     this.x = x;
+    //     this.y = y;
+    //     this.width = width;
+    //     this.height = height;
+    //     this.speedX = 0;
+    //     this.SpeedY = 0;
+    //     this.frames = 0;
+    //     this.obstacles = [];
+    
 
+    //   left() {
+    //     return this.x;
+    //   }
+    
+    //   right() {
+    //     return this.x + this.width;
+    //   }
+    
+    //   top() {
+    //     return this.y;
+    //   }
+    
+    //   bottom() {
+    //     return this.y + this.height;
+    //   }
+    
+    //   isCrashedWith(obstacle) {
+    //     const condition = !(
+    //       this.bottom() < obstacle.top() ||
+    //       this.top() > obstacle.bottom() ||
+    //       this.right() < obstacle.left() ||
+    //       this.left() > obstacle.right()
+    //     );
+    
+    //     return condition;
+    //   }
+    // };
 
+    const obstacles = {
+      blast: blast,
+      x: 0,
+      y: Math.floor(Math.random() * (400 - 100 + 1) + 1),
+      speed: -7.5,
+      frames: 0,
+      obstacle: [],
+  
+      move: function() {
+        this.x += this.speed;
+        this.x %= canvas.width + 400;
+        
+      },
+    
+      draw: function() {
+        ctx.drawImage(this.blast, this.x + canvas.width, this.y, 50, 50);  
+      }
+    
+    };
+
+    
+    
+    function updateObstacle() {
+        obstacles.move();
+        obstacles.draw();
+        requestAnimationFrame(updateObstacle);
+    }  
+
+    //SCORE 
+
+    const score = {
+      score: 0,
+      frames: 0,
+  
+      points: function() {
+        this.frames++;
+        if(this.frames % 20 === 0){
+          this.score++;
+        }
+      },
+    
+      draw: function() {
+          ctx.font = "20px Arial";
+          ctx.fillStyle = "black";
+
+          ctx.fillText(`Score: ${this.score}`, 600, 50);
+      },
+    };
+    
+    function updateScore() {
+        score.points();
+        score.draw();
+        requestAnimationFrame(updateScore);
+    }  
+
+    // window.onload = updateScore;
+  
 
